cd8ab871e6d2807dc45f1a0767af68b4c54897fb
Nghiabe <nghiantk.21it@vku.udn.vn>
2025-04-01 21:34:40 +0700

    Initial commisact

--- resources/views/index_Admin.blade.php
+++ resources/views/index_Admin.blade.php
@@ -5,29 +5,40 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script type="application/x-javascript">
+    addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); }
+</script>
+
 <!-- bootstrap-css -->
-<link rel="stylesheet" href="{{asset('backend/css/bootstrap.min.css')}}" >
+<link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}?v=2" >
 <!-- //bootstrap-css -->
+
 <!-- Custom CSS -->
-<link href="{{asset('backend/css/style.css')}}" rel='stylesheet' type='text/css' />
-<link href="{{asset('backend/css/style-responsive.css')}}" rel="stylesheet"/>
+<link href="{{ asset('backend/css/style.css') }}?v=2" rel='stylesheet' type='text/css' />
+<link href="{{ asset('backend/css/style-responsive.css') }}?v=2" rel="stylesheet"/>
+
 <!-- font CSS -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
 <!-- font-awesome icons -->
-<link rel="stylesheet" href="{{asset('backend/css/font.css')}}" type="text/css"/>
-<link href="{{asset('backend/css/font-awesome.css')}}" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('backend/css/morris.css')}}" type="text/css"/>
+<link rel="stylesheet" href="{{ asset('backend/css/font.css') }}?v=2" type="text/css"/>
+<link href="{{ asset('backend/css/font-awesome.css') }}?v=2" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('backend/css/morris.css') }}?v=2" type="text/css"/>
+
 <!-- calendar -->
-<link rel="stylesheet" href="{{asset('backend/css/monthly.css')}}">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+<link rel="stylesheet" href="{{ asset('backend/css/monthly.css') }}?v=2">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css?version=2">
+
 <!-- //calendar -->
 <!-- //font-awesome icons -->
-<script src="{{asset('backend/js/jquery2.0.3.min.js')}}"></script>
-<script src="{{asset('backend/js/raphael-min.js')}}"></script>
-<script src="{{asset('backend/js/morris.js')}}"></script>
-<script src="Asset/js/onclick.js"></script>
+
+<script src="{{ asset('backend/js/jquery2.0.3.min.js') }}?v=2"></script>
+<script src="{{ asset('backend/js/raphael-min.js') }}?v=2"></script>
+<script src="{{ asset('backend/js/morris.js') }}?v=2"></script>
+<script src="Asset/js/onclick.js?version=2"></script>
 </head>
+
 <body>
 <section id="container">
 <!--header start-->
