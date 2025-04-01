3e7e3684be2d62a6dd3a786aa75a4f411a471642
Nghiabe <nghiantk.21it@vku.udn.vn>
2025-04-01 22:15:09 +0700

    Initial commisacat

--- resources/views/index_Admin.blade.php
+++ resources/views/index_Admin.blade.php
@@ -5,40 +5,30 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript">
-    addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-    function hideURLbar(){ window.scrollTo(0,1); }
-</script>
-
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- bootstrap-css -->
-<link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}?v=2" >
+<link rel="stylesheet" href="{{asset('backend/css/bootstrap.min.css')}}" >
 <!-- //bootstrap-css -->
-
 <!-- Custom CSS -->
-<link href="{{ asset('backend/css/style.css') }}?v=2" rel='stylesheet' type='text/css' />
-<link href="{{ asset('backend/css/style-responsive.css') }}?v=2" rel="stylesheet"/>
-
+<link rel="stylesheet" href="backend/css/style.css">
+<link rel="stylesheet" href="backend/css/style-responsive.css">
 <!-- font CSS -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-
 <!-- font-awesome icons -->
-<link rel="stylesheet" href="{{ asset('backend/css/font.css') }}?v=2" type="text/css"/>
-<link href="{{ asset('backend/css/font-awesome.css') }}?v=2" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('backend/css/morris.css') }}?v=2" type="text/css"/>
-
+<link rel="stylesheet" href="backend/css/font.css">
+<link rel="stylesheet" href="backend/css/font-awesome.css">
+<link rel="stylesheet" href="backend/css/morris.css">
 <!-- calendar -->
-<link rel="stylesheet" href="{{ asset('backend/css/monthly.css') }}?v=2">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css?version=2">
+<link rel="stylesheet" href="backend/css/monthly.css">
 
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 <!-- //calendar -->
 <!-- //font-awesome icons -->
-
-<script src="{{ asset('backend/js/jquery2.0.3.min.js') }}?v=2"></script>
-<script src="{{ asset('backend/js/raphael-min.js') }}?v=2"></script>
-<script src="{{ asset('backend/js/morris.js') }}?v=2"></script>
-<script src="Asset/js/onclick.js?version=2"></script>
+<link rel="stylesheet" href="backend/js/jquery2.0.3.min.js">
+<link rel="stylesheet" href="backend/js/raphael-min.js">
+<link rel="stylesheet" href="backend/js/morris.js">
+<link rel="stylesheet" href="Asset/js/onclick.js">
 </head>
-
 <body>
 <section id="container">
 <!--header start-->
@@ -205,7 +195,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
  <!-- footer -->
 		  <div class="footer">
 			<div class="wthree-copyright">
-			  <p>© 2022 Visitors. All rights reserved | Design by <a href="https://w3layouts.com">D&N Shop</a></p>
+			  <p>© 2022 Visitors. All rights reserved | Design by <a href="http://w3layouts.com">D&N Shop</a></p>
 			</div>
 		  </div>
   <!-- / footer -->
