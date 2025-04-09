c9bab542d4c350a9a2949946d51f9eaddc822eea
Nghiabe <nghiantk.21it@vku.udn.vn>
2025-04-09 10:31:14 +0700

    Fix view nmxjjalkjhga

--- resources/views/index_Admin.blade.php
+++ resources/views/index_Admin.blade.php
@@ -3,31 +3,42 @@
 <title>Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- bootstrap-css -->
-<link rel="stylesheet" href="{{asset('backend/css/bootstrap.min.css')}}" >
-<!-- //bootstrap-css -->
-<!-- Custom CSS -->
-<link rel="stylesheet" href="backend/css/style.css">
-<link rel="stylesheet" href="backend/css/style-responsive.css">
-<!-- font CSS -->
-<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-<!-- font-awesome icons -->
-<link rel="stylesheet" href="backend/css/font.css">
-<link rel="stylesheet" href="backend/css/font-awesome.css">
-<link rel="stylesheet" href="backend/css/morris.css">
-<!-- calendar -->
-<link rel="stylesheet" href="backend/css/monthly.css">
+<meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript">
+    addEventListener("load", function() {
+        setTimeout(hideURLbar, 0);
+    }, false);
+    function hideURLbar(){ window.scrollTo(0,1); }
+</script>
 
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+<!-- bootstrap-css -->
+<link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}">
+<!-- //bootstrap-css -->
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/style-responsive.css') }}">
+
+<!-- font CSS -->
+<link href="//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="{{ asset('backend/css/font.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/font-awesome.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/morris.css') }}">
+<!-- calendar -->
+<link rel="stylesheet" href="{{ asset('backend/css/monthly.css') }}">
 <!-- //calendar -->
 <!-- //font-awesome icons -->
-<link rel="stylesheet" href="backend/js/jquery2.0.3.min.js">
-<link rel="stylesheet" href="backend/js/raphael-min.js">
-<link rel="stylesheet" href="backend/js/morris.js">
-<link rel="stylesheet" href="Asset/js/onclick.js">
+
+<!-- Thêm Font Awesome CDN mới -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+<!-- script load js -->
+<script src="{{ asset('backend/js/jquery2.0.3.min.js') }}"></script>
+<script src="{{ asset('backend/js/raphael-min.js') }}"></script>
+<script src="{{ asset('backend/js/morris.js') }}"></script>
+<script src="{{ asset('Asset/js/onclick.js') }}"></script>
 </head>
 <body>
 <section id="container">
